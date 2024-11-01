f177b43c3dd56b11c353ae0069108784d69fd0f0
MAKSYM SHEPETA <107277579+mikk1max@users.noreply.github.com>
2024-11-01 05:24:38 +0100

    add first db get request

--- app/partials/Sends.jsx
+++ app/partials/Sends.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   StyleSheet,
   View,
@@ -13,6 +13,9 @@ import AdSendCard from "../components/AdSendCard";
 import { ScrollView } from "react-native-gesture-handler";
 import { TouchableOpacity } from "react-native";
 import { FontAwesome6 } from "@expo/vector-icons";
+
+import { ref, onValue } from "firebase/database";
+import { db } from "../../firebaseConfig";
 
 const { width } = Dimensions.get("window");
 
@@ -34,73 +37,90 @@ const statuses = [
 ];
 
 const SendsView = () => {
+  const [products, setProducts] = useState([]);
   const [activeStatus, setActiveStatus] = useState("All");
 
-  const products = [
-    {
-      id: 5,
-      link: "link to sweater",
-      name: "Sweater",
-      price: 8.99,
-      dateFrom: new Date("2024-11-01"),
-      dateTo: new Date("2024-11-06"),
-      status: statuses[1],
-    },
-    {
-      id: 4,
-      link: "link to blue jeans",
-      name: "Blue jeans",
-      price: 40.0,
-      dateFrom: new Date("2024-11-01"),
-      dateTo: new Date("2024-11-04"),
-      status: statuses[2],
-    },
-    {
-      id: 6,
-      link: "link to black socks",
-      name: "Black socks",
-      price: 10.99,
-      dateFrom: new Date("2024-10-31"),
-      dateTo: new Date("2024-11-05"),
-      status: statuses[3],
-    },
-    {
-      id: 1,
-      link: "link to holey underpants",
-      name: "Holey underpants",
-      price: 5.25,
-      dateFrom: new Date("2024-10-27"),
-      dateTo: new Date("2024-10-30"),
-      status: statuses[4],
-    },
-    {
-      id: 2,
-      link: "link to black shoes",
-      name: "Black shoes",
-      price: 1256987.99,
-      dateFrom: new Date("2024-10-20"),
-      dateTo: new Date("2024-11-05"),
-      status: statuses[5],
-    },
-    {
-      id: 3,
-      link: "link to red hat",
-      name: "Red hat",
-      price: 0.1,
-      dateFrom: new Date("2024-10-15"),
-      dateTo: new Date("2024-10-22"),
-      status: statuses[6],
-    },
-    {
-      id: 7,
-      link: "link to green boots",
-      name: "green boots",
-      price: 13.99,
-      dateFrom: new Date("2024-09-15"),
-      dateTo: new Date("2024-09-22"),
-      status: statuses[7],
-    },
-  ];
+  // const products = [
+  //   {
+  //     id: 5,
+  //     link: "link to sweater",
+  //     name: "Sweater",
+  //     price: 8.99,
+  //     dateFrom: new Date("2024-11-01"),
+  //     dateTo: new Date("2024-11-06"),
+  //     status: statuses[1],
+  //   },
+  //   {
+  //     id: 4,
+  //     link: "link to blue jeans",
+  //     name: "Blue jeans",
+  //     price: 40.0,
+  //     dateFrom: new Date("2024-11-01"),
+  //     dateTo: new Date("2024-11-04"),
+  //     status: statuses[2],
+  //   },
+  //   {
+  //     id: 6,
+  //     link: "link to black socks",
+  //     name: "Black socks",
+  //     price: 10.99,
+  //     dateFrom: new Date("2024-10-31"),
+  //     dateTo: new Date("2024-11-05"),
+  //     status: statuses[3],
+  //   },
+  //   {
+  //     id: 1,
+  //     link: "link to holey underpants",
+  //     name: "Holey underpants",
+  //     price: 5.25,
+  //     dateFrom: new Date("2024-10-27"),
+  //     dateTo: new Date("2024-10-30"),
+  //     status: statuses[4],
+  //   },
+  //   {
+  //     id: 2,
+  //     link: "link to black shoes",
+  //     name: "Black shoes",
+  //     price: 1256987.99,
+  //     dateFrom: new Date("2024-10-20"),
+  //     dateTo: new Date("2024-11-05"),
+  //     status: statuses[5],
+  //   },
+  //   {
+  //     id: 3,
+  //     link: "link to red hat",
+  //     name: "Red hat",
+  //     price: 0.1,
+  //     dateFrom: new Date("2024-10-15"),
+  //     dateTo: new Date("2024-10-22"),
+  //     status: statuses[6],
+  //   },
+  //   {
+  //     id: 7,
+  //     link: "link to green boots",
+  //     name: "green boots",
+  //     price: 13.99,
+  //     dateFrom: new Date("2024-09-15"),
+  //     dateTo: new Date("2024-09-22"),
+  //     status: statuses[7],
+  //   },
+  // ];
+
+  useEffect(() => {
+    const productsRef = ref(db, "products");
+    onValue(productsRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        const productsArray = Object.keys(data).map((key) => ({
+          id: key,
+          ...data[key],
+          dateFrom: new Date(data[key].dateFrom),
+          dateTo: new Date(data[key].dateTo),
+        }));
+        setProducts(productsArray);
+      }
+    });
+  }, []);
 
   const filteredProducts = products.filter(
     (product) => activeStatus === "All" || product.status === activeStatus
